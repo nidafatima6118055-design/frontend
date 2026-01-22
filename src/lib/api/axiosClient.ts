e90e53dcbbe00d8e183f54c8f7daadcb0a1ceca0
mamona075-ops <mamona075@gmail.com>
2026-01-22 15:50:15 +0500

    Fixed Create Agent

--- src/lib/api/axiosClient.ts
+++ src/lib/api/axiosClient.ts
@@ -36,7 +36,7 @@ let failedQueue: Array<{
 const processQueue = (error: any, token: string | null = null) => {
     failedQueue.forEach((prom) => {
         if (error) prom.reject(error);
-        else prom.resolve(token);
+        else prom.resolve(token ?? undefined);
     });
     failedQueue = [];
 };
